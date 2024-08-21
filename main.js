b72f9e76f0d262303e73f9127748142c763ad1ba
Tauqeer Ahmed <tauqeer1040@gmail.com>
2024-08-21 22:16:43 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// POSSIBLE FIX, USE PUBLIC FOLDER INSTEAD OF IMAGES FOLDER
+
+// const imageContainer = document.getElementById("gallery");
+
+// // Your images folder path
+// // const imagesFolderPath = "./images/";
+
+// let imageFilenames = [];
+
+// for(let i = 1; i <= 23; i++){
+//     imageFilenames.push(`${i}.jpg`)
+// }
+
+// // Loop through image filenames and create <img> elements
+// imageFilenames.forEach(filename => {
+//     const img = document.createElement("img");
+//     img.src = "/images/" + filename;
+//     img.alt = filename;
+//     img.classList.add("image");
+//     img.setAttribute("data-filename", filename); // Store filename as a data attribute
+//     imageContainer.appendChild(img);
+// });
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 1500,
+      },
+  });
+  
